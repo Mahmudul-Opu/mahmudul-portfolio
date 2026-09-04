46d7505e8f48308352540660d3f0be3a49182745
Mahmudul-Opu <shakibopu030@gmail.com>
2026-09-04 23:29:15 +0600

    Update IUB TechFest name

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -24,7 +24,7 @@ function Experience() {
                     </h3>
 
                     <h4>
-                        IUB CSE Fest
+                        IUB TechFest
                     </h4>
 
                     <p>
